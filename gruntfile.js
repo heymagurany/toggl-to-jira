dfd3ec963ee7cd002c1dfd74edfbf6db907b46bb
Matt Magurany <mmagurany@live.com>
2016-12-15 13:46:07 -0500

    Add command for calculating work log per epic

--- gruntfile.js
+++ gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       all: [
-        "Gruntfile.js",
+        "gruntfile.js",
         "bin/*.js",
         "lib/*.js",
         "test/*.js"
