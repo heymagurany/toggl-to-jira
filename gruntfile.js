28d497518dc4dd93b4feea45a6f7be97f1b0ba3e
Matt Magurany <mmagurany@live.com>
2018-04-06 12:10:42 -0400

    Unlint code

--- gruntfile.js
+++ gruntfile.js
@@ -3,6 +3,9 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
+      options: {
+        jshintrc: true
+      },
       all: [
         "gruntfile.js",
         "bin/*.js",
